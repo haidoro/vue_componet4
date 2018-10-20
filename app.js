c2b033cec50c9d068792ee441e298b861fc4ad4c
Tahara <hydoro-3181@itstudio.co>
2018-10-20 23:31:05 +0900

    slot sample

---
+++ app.js
@@ -0,0 +1,20 @@
+
+var headerTemplate =`<div>
+<slot name="header">No title</slot>
+</div>`
+
+var contentTemplate = `
+<div>
+<slot name="content">No content</slot>
+</div>`
+
+Vue.component('page-header',{
+    template:headerTemplate
+})
+Vue.component('page-content',{
+    template:contentTemplate
+})
+
+new Vue({
+    el:"#fruits-list"
+})
